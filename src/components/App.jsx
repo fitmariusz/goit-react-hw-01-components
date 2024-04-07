87e4ec62e668920519bc05c4cbc3181043168f81
Mariusz Fit <72906237+fitmariusz@users.noreply.github.com>
2024-04-07 22:02:12 +0200

    css in Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,20 +4,22 @@ import { FriendList } from "./FriendList/Friends";
 import user from "./Profile/user.json"
 import { Statistics } from "./Statistics/Statistics";
 import data from "./Statistics/data.json"
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
+import transactions from './TransactionHistory/transactions.json'
 
 export const App = () => {
   return (
     <>
-     <Profile 
+      <Profile 
       username={user.username}
       tag={user.tag}
       location={user.location}
       avatar={user.avatar}
       stats={user.stats}></Profile>
       
-    <Statistics status={data}></Statistics>
+      <Statistics status={data}></Statistics>
 
-    <FriendList friends={friends}></FriendList>
-  </>)
-};
+      <FriendList friends={friends}></FriendList>
+      <TransactionHistory items={transactions}></TransactionHistory>  
+    </>)};
 
